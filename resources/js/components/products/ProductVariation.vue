f78d1622d5f214f7d7ba59459bb4f9248799b147
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-07 23:54:32 +0500

    showing if the product variation price differs

--- resources/js/components/products/ProductVariation.vue
+++ resources/js/components/products/ProductVariation.vue
@@ -3,7 +3,10 @@
     <label>{{ type }}</label>
     <select class="form-control">
       <option>Pleace choose</option>
-      <option v-for="variation in variations" :key="variation.id">{{ variation.name }}</option>
+      <option v-for="variation in variations" :key="variation.id" :value="variation.id">
+        {{ variation.name }}
+        <template v-if="variation.price_varies">({{ variation.price }})</template>
+      </option>
     </select>
   </div>
 </template>
