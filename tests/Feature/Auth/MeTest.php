6536a1731391a1f5b8457af398b40a551738be6d
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-10 16:25:13 +0500

    the me endpoint

---
+++ tests/Feature/Auth/MeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MeTest extends TestCase
+{
+    public function test_it_fails_if_user_isnot_authenticated()
+    {
+        $this->json('GET', 'api/auth/me')
+        ->assertStatus(401);
+    }
+
+    public function test_it_returns_user_detail()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'GET', 'api/auth/me')
+            ->assertJsonFragment([
+            'email' => $user->email
+        ]);
+        
+    }
+}
