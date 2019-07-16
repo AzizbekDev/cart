3266e923158285a8f0a9d3ac2296e1e13e9a659d
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-17 01:23:16 +0500

    toggling default addresses

--- tests/Unit/Models/Addresses/AddressTest.php
+++ tests/Unit/Models/Addresses/AddressTest.php
@@ -26,4 +26,21 @@ class AddressTest extends TestCase
         ]);
         $this->assertInstanceOf(User::class, $address->user);
     }
+
+    public function test_it_set_old_addresses_to_not_default_when_creating()
+    {
+        $user = factory(User::class)->create();
+
+        $oldaddress = factory(Address::class)->create([
+            'default' => true,
+            'user_id' => $user->id
+        ]);
+
+        factory(Address::class)->create([
+            'default' => true,
+            'user_id' => $user->id
+        ]);
+    
+        $this->assertEquals($oldaddress->fresh()->default, 0);
+    }
 }
