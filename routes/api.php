15dcdceb8dd59ff38af79525520c068edfc2ce23
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-16 21:22:27 +0500

    listing addresses

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 Route::resource('categories', 'Categories\CategoryController');
 Route::resource('products', 'Products\ProductController');
+Route::resource('addresses', 'Addresses\AddressController');
+
 Route::group(['prefix' => 'auth'], function(){
     Route::post('register', 'Auth\RegisterController@action');
     Route::post('login', 'Auth\LoginController@action')->name('login');
