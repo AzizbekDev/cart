7ae6783d4201d9cc7403a4e40aab8d485b2f21d9
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-16 22:07:39 +0500

    storing an address

---
+++ tests/Feature/Addresses/AddressStoreTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Feature\Addresses;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Country;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AddressStoreTest extends TestCase
+{
+
+    public function test_it_fails_if_authenticated()
+    {
+        $this->json('POST', 'api/addresses')
+        ->assertStatus(401);
+    }
+
+    public function test_it_requires_a_name()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses')
+        ->assertJsonValidationErrors('name');
+    }
+
+    public function test_it_requires_address_line_one()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses')
+        ->assertJsonValidationErrors('address_1');
+    }
+
+    public function test_it_requires_city()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses')
+        ->assertJsonValidationErrors('city');
+    }
+
+    public function test_it_requires_postal_code()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses')
+        ->assertJsonValidationErrors('postal_code');
+    }
+
+    public function test_it_requires_a_country()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses')
+        ->assertJsonValidationErrors('country_id');
+    }
+
+    public function test_it_requires_a_valid_country()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses',[
+            'country_id' => 1
+        ])
+        ->assertJsonValidationErrors('country_id');
+    }
+
+    public function test_it_stores_an_address()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/addresses', $payload = [
+            'name' => 'TestName',
+            'address_1' => 'TestAddress',
+            'city' => 'TestCity',
+            'postal_code' => '12345',
+            'country_id' => factory(Country::class)->create()->id
+        ]);
+        
+        $this->assertDatabaseHas('addresses', array_merge($payload,[
+            'user_id' => $user->id
+            ])
+        );
+    }
+
+    public function test_it_returns_an_address_when_created()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->jsonAs($user, 'POST', 'api/addresses', $payload = [
+            'name' => 'TestName',
+            'address_1' => 'TestAddress',
+            'city' => 'TestCity',
+            'postal_code' => '12345',
+            'country_id' => factory(Country::class)->create()->id
+        ]);
+        $response->assertJsonFragment([
+            'id' => json_decode($response->getContent())->data->id
+        ]);
+    }
+}
