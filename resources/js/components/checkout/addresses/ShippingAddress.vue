5b14546d21ee2e4deca8bb3ebfbdad765931f7b4
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-07-19 20:00:09 +0500

    getting selectedAddress form props error fixed

--- resources/js/components/checkout/addresses/ShippingAddress.vue
+++ resources/js/components/checkout/addresses/ShippingAddress.vue
@@ -1,11 +1,49 @@
 <template>
-  <div class="border border-left-0" v-if="selectedAddress">
-    {{ selectedAddress }}
-    <br />
-  </div>
-  <div v-else>no address</div>
+  <article class="pl-4">
+    <template v-if="selecting">
+      <ShippingAddressSelector
+        :addresses="addresses"
+        :selectedAddress="selectedAddress"
+        @click="addressSelected"
+      />
+    </template>
+    <template v-else>
+      <template v-if="selectedAddress">
+        <p>
+          <b>Name:</b>
+          {{ selectedAddress.name }}
+          <br />
+          <b>Address:</b>
+          {{ selectedAddress.address_1 }}
+          <br />
+          <b>City:</b>
+          {{ selectedAddress.city }}
+          <br />
+          <b>Postalcode:</b>
+          {{ selectedAddress.postal_code }}
+          <br />
+          Country: {{ selectedAddress.country.name }}
+        </p>
+        <br />
+      </template>
+      <template v-else>
+        <p>No address</p>
+        <br />
+      </template>
+      <div class="field">
+        <p>
+          <button
+            type="button"
+            @click.prevent="selecting = true"
+            class="btn btn-primary btn-sm"
+          >Change Address</button>
+        </p>
+      </div>
+    </template>
+  </article>
 </template>
 <script>
+import ShippingAddressSelector from "../addresses/ShippingAddressSelector";
 export default {
   props: {
     addresses: {
@@ -15,24 +53,41 @@ export default {
   },
   data() {
     return {
-      localAddresses: this.addresses,
+      selecting: false,
       selectedAddress: null
     };
   },
+  components: {
+    ShippingAddressSelector
+  },
+  watch: {
+    defaultAddress(v) {
+      if (v) {
+        this.switchAddress(v);
+      }
+    }
+  },
   computed: {
     defaultAddress() {
-      return this.localAddresses.find(a => a.default === 1);
+      return this.addresses.find(a => a.default === 1);
     }
   },
   methods: {
+    addressSelected(address) {
+      this.switchAddress(address);
+      this.selecting = false;
+    },
     switchAddress(address) {
       this.selectedAddress = address;
-    }
-  },
-  created() {
-    if (this.addresses.length) {
-      this.switchAddress(this.defaultAddress);
     }
   }
 };
 </script>
+<style scoped>
+article {
+  border-left: 1px solid gray;
+}
+.field {
+  display: block;
+}
+</style>
